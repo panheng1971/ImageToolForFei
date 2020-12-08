dfa0fd9080cafd8a9cb0dbaf3be56118f19b084a
panheng1971 <pan.heng@hotmail.com>
2020-12-08 09:42:58 +0800

    ok

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "显示原始图像";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(124, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "计算比值";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "图像1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(448, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "图像2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(505, 22);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(448, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1784, 861);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "图像比率处理-潘恒研制";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp1; 
+        Bitmap bmp2;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "1.png";
+            textBox2.Text = "2.png";
+        }
+   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" & textBox2.Text == "")
+            {
+                MessageBox.Show("please input the image file name (include folders)");
+                return;
+            }
+
+            bmp1 = new Bitmap(textBox1.Text.ToString());
+            bmp2 = new Bitmap(textBox2.Text.ToString());
+            Graphics g = CreateGraphics();
+            g.DrawImage(bmp1,new Rectangle(10,120,bmp1.Width,bmp1.Height));
+            g.DrawImage(bmp2, new Rectangle(10+bmp1.Width+10, 120, bmp2.Width, bmp2.Height));
+            g.Dispose();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" & textBox2.Text == "") {
+                MessageBox.Show("please input the image file name (include folders)");
+                return;
+            }
+            bmp1 = new Bitmap(textBox1.Text.ToString());
+            bmp2 = new Bitmap(textBox2.Text.ToString());
+            byte[] info1 = GetImagePixel(bmp1);
+            byte[] info2 = GetImagePixel(bmp2);
+            byte[] info3 = info1;
+
+            for (int num=0;num<info3.Length;num+=3)
+            {
+                info3[num] = 125;
+                info3[num + 2] = 125;
+                if (info2[num + 1] != 0 & info1[num + 1] != 0)
+                {
+                    Console.WriteLine( info2[num + 1] / info1[num + 1]);
+                    var t = info2[num + 1] / info1[num + 1];
+                    info3[num + 1] = (byte)(t*20);
+                }
+            }
+            Bitmap bmp3 = SetImagePixel(bmp2, info3);
+            Graphics g = this.CreateGraphics();
+            g.DrawImage(bmp2, new Rectangle(10 + bmp1.Width+10 +bmp1.Width+ 10, 120, bmp2.Width, bmp2.Height));
+            g.Dispose();
+        }
+
+
+        public byte[] GetImagePixel(Bitmap img)
+        {
+            byte[] result = new byte[img.Width * img.Height * 3];
+            int n = 0;
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+                    result[n] = img.GetPixel(i, j).R;
+                    result[n + 1] = img.GetPixel(i, j).G;
+                    result[n + 2] = img.GetPixel(i, j).B;
+                    n += 3;
+                    
+                }
+            }
+            return result;
+        }
+
+        public Bitmap SetImagePixel(Bitmap img,byte[] info)
+        {
+           
+            int n = 0;
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+                  
+                    img.SetPixel(i, j, Color.FromArgb(info[n],info[n+1],info[n+2]));
+                    n += 3;
+                
+                }
+            }
+            return img;
+         }
+    }
+}
